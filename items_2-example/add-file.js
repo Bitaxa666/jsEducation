ae207c3308fe6a91212c36691df7a649fe2e87e7
Vitaliy QAFreshcode <vmihaylyuk@freshcode.org>
2018-11-05 18:43:07 +0200

    created the preview page and script

--- items_2-example/add-file.js
+++ items_2-example/add-file.js
@@ -1,24 +1,32 @@
 const myBtnClass = document.querySelector(".my");
 const file = document.querySelector("#myfile");
 
-// $('.my').change(function() {
-//     if ($(this).val() != '') $(this).prev().text('Выбрано файлов: ' + $(this)[0].files.length);
-//     else $(this).prev().text('Выберите файлы');
-// });
-// const targetItem = e.target;
-//     const targetChildrenItem = targetItem.parentNode.children;
-//     if(targetItem.nodeName == "LI") {
-//         ourHeadline.innerHTML = targetItem.innerHTML;
-//         for(let i = 0; i < targetChildrenItem.length; i++){
-//             targetChildrenItem[i].classList.remove("active");
-//             if(targetChildrenItem[i] == targetItem){
-//                 targetItem.classList.add("active");
-//                 deleteIndex = i;
-//                 continue;
-//             }
-//         }
-//         selectItem = targetItem;
+// var slider = {
+//     slides: ['6.jpg', '9.jpg', '20.jpg'],
+//     frame:0,
+//     set: function (image) {
+//         document.getElementById("scr").style.backgroundImage = "url(images/" + image + ")";
+//     },
+//     init: function () {
+//         this.set(this.slides[this.frame]);
+//     },
+//     left: function () {
+//         this.frame--;
+//         if(this.frame < 0) this.frame = this.slides.length -1;
+//         this.set(this.slides[this.frame]);
+//     },
+//     right: function () {
+//         this.frame++;
+//         if(this.frame == this.slides.length) this.frame = 0;
+//         this.set(this.slides[this.frame]);
 //     }
+// };
+// window.onload = function () {
+//     slider.init();
+//     setInterval(function () {
+//         slider.right();
+//     }, 5000);
+// };
 // http://qaru.site/questions/11050/preview-an-image-before-it-is-uploaded
 // 
 
@@ -32,6 +40,19 @@ function addImage(e){
 
 
 	for (var i = 0; i < file.files.length; i++) {
+        console.log(file.files[i].name);
+        console.log(file.files[i].src + 2);
+    }
+}
+
+function addOnlyImage(){
+    const previewForm = document.querySelector("#img-test");
+    const fileReader = document.querySelector("#input-test-prev").files[0];
+    //let file    = document.querySelector('input[type=file]').files[0];
+    let reader = new FileReader;
+
+
+    for (var i = 0; i < file.files.length; i++) {
         console.log(file.files[i].name);
         console.log(file.files[i].src + 2);
     }
